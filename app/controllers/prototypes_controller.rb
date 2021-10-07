7af51441ec98841489d0aa7076698aa6aebbaa0d
takayun <beachshot10@gmail.com>
2021-10-07 17:38:14 +0900

    プロトタイプ編集機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_prototype, only: [:edit, :show, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user).order("updated_at DESC")
@@ -19,11 +21,20 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to prototypes_path
     end
@@ -33,6 +44,16 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:price, :car_maker, :car_model, :car_grade, :body_color, :vi_number, :year, :mileage, :drive_system, :transmission, :inspection, :comment, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def correct_user
+    if current_user.id != @prototype.user_id
+      redirect_to root_path
+    end
   end
 
 end
