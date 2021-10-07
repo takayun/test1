d284eec0669f4bd566e59d7407c32d37136c54fe
takayun <beachshot10@gmail.com>
2021-10-07 12:51:10 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def basic_auth
     if Rails.env.production?
       authenticate_or_request_with_http_basic do |username, password|
-        username == 'trader' && password == '4141'
+        username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述
       end
     end
   end
